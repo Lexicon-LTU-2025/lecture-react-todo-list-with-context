0733ada5e474fbafb41f3207868a64c8a3b0725e
Niklas Fähnrich <niklas.fahnrich@safeture.com>
2025-09-05 10:33:07 +0200

    implement local storage in favorites context

--- src/features/favorites/context/FavoriteContextProvider.tsx
+++ src/features/favorites/context/FavoriteContextProvider.tsx
@@ -1,4 +1,4 @@
-import { useState, type ReactElement, type ReactNode } from 'react';
+import { useEffect, useState, type ReactElement, type ReactNode } from 'react';
 import { FavoriteContext, type IFavoriteContext } from '.';
 import type { ITodo } from '../../todos';
 
@@ -9,7 +9,10 @@ interface IFavoriteContextProviderProps {
 export const FavoriteContextProvider = ({
   children,
 }: IFavoriteContextProviderProps): ReactElement => {
-  const [favorites, setFavorites] = useState<ITodo[]>([]);
+  const [favorites, setFavorites] = useState<ITodo[]>(() => {
+    const stored = localStorage.getItem('favorites');
+    return stored ? (JSON.parse(stored) as ITodo[]) : [];
+  });
 
   const add = (todo: ITodo): void => setFavorites([todo, ...favorites]);
   const checkIfFavorite = (todo: ITodo): boolean => favorites.includes(todo);
@@ -21,6 +24,10 @@ export const FavoriteContextProvider = ({
     checkIfFavorite,
     remove,
   };
+
+  useEffect(() => {
+    localStorage.setItem('favorites', JSON.stringify(favorites));
+  }, [favorites]);
 
   return <FavoriteContext.Provider value={values}>{children}</FavoriteContext.Provider>;
 };
